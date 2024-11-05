9838da01b83ec53b6c226b6ec4b7e1339fceea20
nagaraj6618 <nagaraj516700@gmail.com>
2024-11-05 18:31:49 +0530

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -22,8 +22,8 @@ function App() {
     });
   };
   function checkLogginStatus() {
-    const userName = sessionStorage.getItem("userName") === "Sec2025";
-    const password = sessionStorage.getItem("password") === "2025";
+    const userName = sessionStorage.getItem("userName") === "FakeSec2025";
+    const password = sessionStorage.getItem("password") === "Fake2025";
     setIsLoggedIn(false);
     if (userName && password) {
       setIsLoggedIn(true);
